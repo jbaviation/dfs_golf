da3e9e1118c822d6de286cea9b28208951c43e87
Jon Borman <jborman1225@gmail.com>
2021-07-21 15:34:27 -0400

    Redesigned scrape.py
    scrape.py works more efficiently with mapping.py and user inputs. User can now go from an external file and pull the dataframe directly from the pgatour.com stats pages. Program currently works better if stat_id is declared as it is difficult to match text directly. Future plan is to update the case matching for the statistic of interest.

--- pga_data/scrape.py
+++ pga_data/scrape.py
@@ -1,47 +1,78 @@
 import pandas as pd
 import numpy as np
-import pga_data.mapping
+import datetime as dt
+import pga_data.mapping as mapping
 
 class stat:
-    def __init__(self, statistical_category):
-        """This class is setup to provide a means for someone to request a particular stat and then              it returns the url from PGATour.com to access the data."""
-        self._cat, self._url_number = pga_data.mapping.interpret_stat(statistical_category)
+    def __init__(self, stat='', stat_id='0', year=dt.datetime.now().year):
+        """This class is setup to provide a means for someone to request a particular stat and then
+        it returns the url from PGATour.com to access the data."""
         
-        # Set base url for whenever url_number changes
-        self.base_url = 'https://www.pgatour.com/stats/stat.{}.html'
+        # Write input values to self
+        self._stat, self._stat_id = self._update_stat(stat, str(stat_id))
+               
+        # Set url for initial class conditions
+        self.season = dt.datetime.now().year
+        self._set_url()
+
         
-        # Set url
-        self.url = self.base_url.format(self._url_number)
+    def _update_stat(self, stat, stat_id='0'):
+        # Look for if stat_id exists
+        if str(stat_id) in list(mapping.list_stats_from_cat().values()):
+            # Set stat and stat_id if stat_id matches what is in the full stat list
+            new_stat, new_stat_id = list({k: v for k, v in mapping.list_stats_from_cat().items() 
+                                            if v==str(stat_id)}.items())[0]
+        else:
+            # Use statistical_category to set cat and cat_id
+            new_stat, new_stat_id = list(mapping.list_stat(stat).items())[0]
+        
+        return new_stat, new_stat_id
+        
+        
+    def _set_url(self):
+        '''Function to be called whenever statistic, season, time period, or tournament 
+           has been changed.  For now this function just accepts seasons and categories.'''
+
+        # Find season to determine base_url
+        this_year = dt.datetime.now().year  # for now set this to be calendar year, eventually will be seasons
+        if self.season == this_year:
+            # Set url for current season
+            base_url = 'https://www.pgatour.com/stats/stat.{}.html'
+            self.url = base_url.format(self.stat_id)
+        else:
+            # Set url for entire alternate season
+            base_url = 'https://www.pgatour.com/content/pgatour/stats/stat.{}.y{}.html'
+            self.url = base_url.format(self.stat_id, self.season)
+            
+    
+    
+    @property
+    def stat(self):
+        return self._stat
+    
+    @stat.setter
+    def stat(self, new_stat):
+        self._stat, self._stat_id = self._update_stat(stat=new_stat)
+        self._set_url()
         
     @property
-    def cat(self):
-        return self._cat
+    def stat_id(self):
+        return self._stat_id
     
-    @cat.setter
-    def cat(self, new_cat):
-        self._cat, self.url_number = pga_data.mapping.interpret_stat(new_cat)
+    @stat_id.setter
+    def stat_id(self, new_stat_id):
+        self._stat, self._stat_id = self._update_stat(stat=None, stat_id=str(new_stat_id))
+        self._set_url()
         
     @property
     def category(self):
         """Define a category variable for easier readability"""
-        return self.cat
+        return self.stat
     
-    @property
-    def url_number(self):
-        return self._url_number
-    
-    @url_number.setter
-    def url_number(self, new_url):
-        self._url_number = new_url
-        self.url = self.base_url.format(self._url_number)
-        
     def pull(self):
         """Function to pull data from pgatour.com and put into dataframe"""
         return pd.read_html(self.url)[1]
     
-    @staticmethod
-    def stat_matches(statistical_category, number_of_matches=5):
-        pass
-    
+
 
 
